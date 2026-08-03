bdcc200acb9084627bcb21e7e1fd94790a59ba9e
bosichong <bosichong@qq.com>
2026-08-03 12:49:58 +0800

    更新

--- src/config/site.ts
+++ src/config/site.ts
@@ -159,4 +159,5 @@ export const extraBloggers = [
   { blog: "Liang's Blog", author: 'Liang', type: '综合', lv: 1, url: 'https://in-x.cc' },
   { blog: '风·墨', author: 'Vind', type: '综合', lv: 1, url: 'https://Vind.ink' },
   { blog: '老姚笔记', author: '夕阳西下', type: '综合', lv: 1, url: 'https://yirz.cn' },
+  { blog: '阿锋的小宇宙', author: '阿锋', type: '综合', lv: 1, url: 'https://blog.feng.pub' },
 ];
